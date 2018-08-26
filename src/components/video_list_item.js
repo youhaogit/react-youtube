105bfbb33a952d84bb666672dc5a1da0cfc10d4d
YouHaoGit <youhaouci@gmail.com>
2018-08-26 11:08:16 -0700

    init commit

---
+++ src/components/video_list_item.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const VideoListItem = ({video}) => {
+    console.log(video);
+  return (
+      <li className="list-group-item">
+          <div className="video-list media">
+
+          </div>
+      </li>
+  );
+};
+
+export default VideoListItem;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyDCnYgZ3idxznNz1gyxFaG94ewHRfiFS_U';
 
 
-// Create a new component
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({videos});
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
 
